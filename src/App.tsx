e503360a354f21ed3b875098dd3a15628f09245c
Flibouche <kevin.pfiffer2@gmail.com>
2024-11-14 14:46:34 +0100

    📦 NEW: add countdown before christmas with react-countdown.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Countdown from "react-countdown";
 import CalendarCase from "./_components/CalendarCase"
 
 // Utilisation de l'algorithme de Fisher-Yates pour mélanger les nombres
@@ -19,7 +20,14 @@ function App() {
             <video className="absolute w-full object-fill -z-10" autoPlay muted loop>
                 <source src="/public/celeste.mp4" />
             </video>
-            <h1 className="text-4xl font-bold text-white text-center">Advent Calendar Geek Edition</h1>
+            <h1 className="text-5xl my-5 font-bold text-white text-center">Advent Calendar Geek Edition</h1>
+            <div className="relative">
+                <div className="absolute bottom-0 -left-5 h-[27px] w-[250px] bg-black -z-10 -skew-x-[20deg]"></div>
+                <Countdown
+                    date={new Date(2024, 12, 24)}
+                    className="text-stroke text-4xl text-white font-bold ml-5 z-10"
+                />
+            </div>
             <div className="grid grid-cols-2 place-items-center h-screen">
                 <div></div>
                 <div className="grid grid-cols-6 gap-4 mr-5">
