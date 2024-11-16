17ced46ef969b6a0c99c04c30c847cb02ec6f00e
Flibouche <kevin.pfiffer2@gmail.com>
2024-11-16 15:37:58 +0100

    📦 NEW: add handleBackgroundClick on the CalendarCaseWindow.

--- src/_components/CalendarCaseWindow.tsx
+++ src/_components/CalendarCaseWindow.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent } from "react";
 import { gameData } from "../data/gameData";
 import { CalendarCaseWindowProps } from "../lib/types/types"
 
@@ -10,8 +11,17 @@ const CalendarCaseWindow = ({ number, onClose }: CalendarCaseWindowProps) => {
 
     const content = getContent(); // Je stocke le contenu de la case dans une variable
 
+    const handleBackgroundClick = (event: MouseEvent<HTMLDivElement>) => {
+        if (event.target === event.currentTarget) {
+            onClose();
+        }
+    }
+
     return (
-        <div className="absolute inset-0 flex justify-center items-center z-[10000]">
+        <div
+            className="absolute inset-0 flex justify-center items-center z-[10000]"
+            onClick={handleBackgroundClick}
+        >
             <div className="relative flex flex-col rounded-lg w-[250px] h-[500px] bg-red-500">
                 <p
                     className="absolute top-0 right-0 mr-2 cursor-pointer"
