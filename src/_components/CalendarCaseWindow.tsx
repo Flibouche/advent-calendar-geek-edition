a07546e818b9595a1715c62784a25649609ce11f
Flibouche <kevin.pfiffer2@gmail.com>
2024-11-29 18:28:39 +0100

    📦 NEW: Add Steam and Itch.io SVG icons; enhance CalendarCaseWindow with 'See more' functionality for descriptions and improved styling.

--- src/_components/CalendarCaseWindow.tsx
+++ src/_components/CalendarCaseWindow.tsx
@@ -2,6 +2,7 @@ import { MouseEvent } from "react";
 import { gameData } from "../data/gameData";
 import { CalendarCaseWindowProps } from "../lib/types/types";
 import Confetti from 'react-confetti';
+import { FaEye } from "react-icons/fa";
 
 const CalendarCaseWindow = ({ number, onClose }: CalendarCaseWindowProps) => {
 
@@ -19,9 +20,16 @@ const CalendarCaseWindow = ({ number, onClose }: CalendarCaseWindowProps) => {
         }
     }
 
+    const handleSeeMore = () => {
+        const description = document.querySelector('.description');
+        const seeMore = document.querySelector('.see-more');
+        description?.classList.remove('line-clamp-2');
+        seeMore?.remove();
+    }
+
     return (
         <div
-            className="absolute inset-0 flex justify-center items-center z-[10000]"
+            className="fixed inset-0 flex justify-center items-center z-[10000] bg-black bg-opacity-50"
             onClick={handleBackgroundClick}
         >
             <Confetti
@@ -30,18 +38,52 @@ const CalendarCaseWindow = ({ number, onClose }: CalendarCaseWindowProps) => {
                 numberOfPieces={300}
                 recycle={false}
             />
-            <div className="relative flex flex-col rounded-lg w-[250px] h-[500px] bg-red-500">
-                <p
-                    className="absolute top-0 right-0 mr-2 cursor-pointer"
+            <div className="relative flex flex-col rounded-xl w-[90%] max-w-md backdrop-blur bg-white/30 shadow-lg p-6">
+                <button
+                    className="absolute top-1 right-4 text-4xl text-black hover:bg-gradient-to-r from-[#FF675E] via-[#FF44EC] to-[#44BCFF] hover:text-transparent hover:bg-clip-text duration-300"
                     onClick={onClose}
                 >
-                    X
-                </p>
+                    &times;
+                </button>
                 {content && (
-                    <div className="">
-                        <h2>{content.number}</h2>
-                        <h3>{content.game}</h3>
-                        <p>{content.description}</p>
+                    <div className="flex flex-col items-center justify-center space-y-2">
+                        <img
+                            src={content.imgUrl}
+                            alt={content.game}
+                            className="rounded-2xl"
+                        />
+                        <h2 className="text-3xl font-bold capitalize mb-4">
+                            {content.game}
+                        </h2>
+                        <p className="description text-black line-clamp-2 mb-4">{content.description}</p>
+                        {content.description.length > 100 && (
+                            <div className="flex w-full">
+                                <button
+                                    className="see-more flex flex-row gap-2 items-center bg-black/20 py-1 px-2 rounded-lg hover:bg-white/60 transition duration-300"
+                                    onClick={handleSeeMore}
+                                >
+                                    <FaEye className="w-[15px]" />
+                                    <span>See more</span>
+                                </button>
+                            </div>
+                        )}
+                        <div className="flex flex-row gap-2">
+                            {content.steamLink && (
+                                <a href={content.steamLink} target="_blank" className="hover:-translate-y-1 duration-300">
+                                    <img src="/steam.svg" alt="Steam logo" />
+                                </a>
+                            )}
+                            {content.epicLink && (
+                                <a href={content.epicLink} target="_blank" className="hover:-translate-y-1 duration-300">
+                                    <img src="/epic.svg" alt="Epic logo" />
+                                </a>
+                            )}
+                            {content.itchLink && (
+                                <a href={content.itchLink} target="_blank" className="hover:-translate-y-1 duration-300">
+                                    <img src="/itch.svg" alt="Itch.io logo" />
+                                </a>
+                            )}
+                        </div>
                     </div>
                 )}
             </div>
@@ -49,4 +91,4 @@ const CalendarCaseWindow = ({ number, onClose }: CalendarCaseWindowProps) => {
     )
 }
 
-export default CalendarCaseWindow
+export default CalendarCaseWindow;
